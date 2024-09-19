3b6a7586aad40e95eaa215e32583105b74d279d3
Ammar Hazim <ammar@pinc.my>
2024-09-19 15:22:40 +0800

    Graph nearly complete, only dropdown to select graph type not complete

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,17 +1,92 @@
 import { LineChart } from "@mui/x-charts/LineChart";
-import { useEffect } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { ATON_GRAPH_DATA } from "../mock/mock";
+import { Msg6 } from "../declarations/types/types";
+
+type GraphDataMap = {
+  [key in keyof Pick<
+    Msg6,
+    "volt_int" | "volt_ex1" | "volt_ex2" | "light" | "beat" | "ambient"
+  >]: string;
+};
+
+const GRAPH_DATA_MAP: GraphDataMap = {
+  volt_int: "ARMS Battery",
+  volt_ex1: "ATON Battery",
+  volt_ex2: "Battery Lantern Primary",
+  light: "Light Sensor",
+  beat: "Heart Beat Sensor",
+  ambient: "Ambient Light Sensor",
+};
 
 export default function Graph() {
-  useEffect(() => {}, []);
+  const [graphType, setGraphType] =
+    useState<keyof typeof GRAPH_DATA_MAP>("volt_ex1");
+  const [xAxisData, setXAxisData] = useState<string[]>([]);
+  const [atonGraphData, setAtonGraphData] = useState<number[]>([]);
+
+  useEffect(() => {
+    const msg6Data = ATON_GRAPH_DATA.msg6;
+
+    const timestamps: string[] = msg6Data.map((msg) => msg.ts_iso).slice(0, 10);
+    const data: number[] = msg6Data
+      .map((msg) => msg[graphType])
+      .slice(0, 10);
+
+    setXAxisData(timestamps);
+    setAtonGraphData(data);
+  }, [graphType]);
+
+  const handleGraphTypeChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setGraphType(event.target.value as keyof typeof GRAPH_DATA_MAP);
+  };
 
   return (
-    <div className="m-auto h-auto z-50">
+    <div className="bg-gray-700 m-4 h-[50vh] z-50">
+      <div className="mb-4 z-50">
+        <label htmlFor="graphType" className="mr-2 text-white">
+          Select Graph Type:
+        </label>
+        <select
+          id="graphType"
+          value={graphType}
+          onChange={handleGraphTypeChange}
+          className="p-2 rounded bg-gray-700 text-white"
+        >
+          {/* Dynamically create dropdown options */}
+          {Object.entries(GRAPH_DATA_MAP).map(([key, label]) => (
+            <option key={key} value={key}>
+              {label}
+            </option>
+          ))}
+        </select>
+      </div>
       <LineChart
         className="bg-gray-800 rounded-md"
-        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        xAxis={[
+          {
+            data: xAxisData.map((x) => new Date(x)),
+            label: "Timestamp",
+            scaleType: "time",
+            valueFormatter: (date) =>
+              date.toLocaleDateString("fr-FR", {
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+              }),
+            tickLabelStyle: { fill: "white" },
+          },
+        ]}
+        yAxis={[
+          {
+            tickLabelStyle: { fill: "white" },
+          },
+        ]}
         series={[
           {
-            data: [2, 5.5, 2, 8.5, 1.5, 5],
+            data: atonGraphData,
+            label: GRAPH_DATA_MAP[graphType],
+            color: "red",
           },
         ]}
       />
